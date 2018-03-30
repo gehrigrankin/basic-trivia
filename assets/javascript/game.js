b161ea0a1704eb422bc7a280b23f05246feb4665
gehrig rankin <rgrankin22@gmail.com>
2018-03-29 17:59:16 -0700

    game is done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,3 @@
-
-
-
 window.onload = function() {
     $('#trivia-game').hide();
     $('#done-window').hide();
@@ -8,23 +5,24 @@ window.onload = function() {
 };
 
 var count = 60;
+
+
 var timer = $('#timer');
+var correct = 0;
+var incorrect = 0;
+var blank = 0;
+
+
 timer.html(count);
 
 setInterval(timeIt, 1000);
-
-
 
 
 if (count == 0){
     done();
 }
 
-function done(){
-    $('#trivia-game').hide();
-    $('#done-window').show();
 
-}
 $('#submit-btn').on('click', done);
 
 $('#restart-btn').on('click', function(){
@@ -33,6 +31,98 @@ $('#restart-btn').on('click', function(){
 
 
 
+
+function done(){
+
+
+    var checkboxes = document.getElementsByName('question1');
+    var vals1 = "";
+    for (var i=0; i<checkboxes.length;i++) 
+    {
+        if (checkboxes[i].checked) 
+        {
+            vals1 += checkboxes[i].value;
+        }
+    }
+
+    
+    if (vals1 == 15){
+        correct++;
+
+        console.log('question 1 is right');
+    }
+    else if (vals1 == ""){
+        blank++;
+
+        console.log('question 1 is blank');
+    }
+    else {
+        incorrect++;
+
+        console.log('question 1 is wrong');
+    }
+
+
+    if ($('#question-2').find('input:checked').val() == 1){
+        correct++;
+
+        console.log('question 2 is right');
+    }
+    else if ($('#question-2').find('input:checked').val() == undefined){
+        blank++;
+
+        console.log('question 2 is blank');
+    }
+    else {
+        incorrect++;
+
+        console.log('question 2 is wrong');
+    }
+
+    if ($('#question-3').find('input:checked').val() == 3){
+        correct++;
+
+        console.log('question 3 is right');
+    }
+    else if ($('#question-3').find('input:checked').val() == undefined){
+        blank++;
+
+        console.log('question 3 is blank');
+    }
+    else {
+        incorrect++;
+
+        console.log('question 3 is wrong');
+    }
+
+    if ($('#question-4').find('input:checked').val() == 3){
+        correct++;
+
+        console.log('question 4 is right');
+    }
+    else if ($('#question-4').find('input:checked').val() == undefined){
+        blank++;
+
+        console.log('question 4 is blank');
+    }
+    else {
+        incorrect++;
+
+        console.log('question 4 is wrong');
+    }
+
+
+    $('#trivia-game').hide();
+    $('#done-window').show();
+
+    $('#num-correct').html(correct);
+    $('#num-incorrect').html(incorrect);
+    $('#num-blank').html(blank);
+    
+    
+    
+
+}
 
 function timeIt(){
     if (count > 0){
@@ -47,6 +137,8 @@ function timeIt(){
 function startGame() {
     $('#trivia-game').show();
     $('#start-window').hide();
+
+    setInterval(timeIt, 1000);
 }
 
 
